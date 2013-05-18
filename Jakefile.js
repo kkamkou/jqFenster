10c3315330245d75eee3dba29cef382b7795a5e7
Kanstantsin Kamkou <kkamkou@gmail.com>
2013-05-18 16:07:58 +0200

    structure polish

--- Jakefile.js
+++ Jakefile.js
@@ -1,48 +1,49 @@
-// npm install uglify-js@1.3.4
-// and deps.
+// required:
+//  npm install uglify-js@1
+//  npm install csso
 
 // nodejs env check
 if (!process.env.NODE_PATH) {
-    console.log('$NODE_PATH is undefined!');
-    console.log('Run: export NODE_PATH="__NODEJS_MODULES__"');
-    console.log('Example: export NODE_PATH="/usr/lib/node_modules" or "/usr/local/lib/node_modules"');
-    console.log('Or: export NODE_PATH="npm root"');
-    process.exit(1);
+  console.log('$NODE_PATH is undefined!');
+  console.log('Run: export NODE_PATH="__NODEJS_MODULES__"');
+  console.log('Example: export NODE_PATH="/usr/lib/node_modules" or "/usr/local/lib/node_modules"');
+  console.log('Or: export NODE_PATH="npm root"');
+  process.exit(1);
 }
 
 // namespaces
 var fs   = require('fs'),
-    ugp  = require('uglify-js').parser,
-    ugu  = require('uglify-js').uglify,
-    csso = require('csso');
+  ugp  = require('uglify-js').parser,
+  ugu  = require('uglify-js').uglify,
+  csso = require('csso');
 
 // defaults
 var files = {
-        'jquery.fenster.full.js': [
-            'jquery.fenster.js',
-            'jquery.fenster-helper.js',
-            'jquery.fenster-helper-template-table.js',
-            'jqEbony/jquery.ebony.js'
-        ],
-        'jquery.fenster.full.css': [
-            'jquery.fenster.css',
-            'jqEbony/fix-ie.css',
-            'jqEbony/fix-ipad.css'
-        ]
-    };
+    'jquery.fenster.full.js': [
+      'src/jquery.fenster.js',
+      'src/jquery.fenster-helper.js',
+      'src/jquery.fenster-helper-template-table.js',
+      'jqEbony/jquery.ebony.js'
+    ],
+    'jquery.fenster.full.css': [
+      'src/jquery.fenster.css',
+      'jqEbony/fix-ie.css',
+      'jqEbony/fix-ipad.css'
+    ]
+  };
 
 function getComment () {
-    var version = '1.2',
-        date = new Date(),
-        buildDate = date.getFullYear() + '-' +
-            (date.getMonth() >= 9 ? '' : '0') + (date.getMonth() + 1) + '-' +
-            (date.getDate() >= 10 ? '' : '0') + date.getDate();
+  var version = '1.2',
+    date = new Date(),
+    buildDate = date.getFullYear() + '-' +
+      (date.getMonth() >= 9 ? '' : '0') + (date.getMonth() + 1) + '-' +
+      (date.getDate() >= 10 ? '' : '0') + date.getDate();
 
-    return '/**\n' +
-        ' * jqFenster - Lightweight Modal Framework\n' +
-        ' * Version: ' + version + ' (' + buildDate + ')\n' +
-        ' * https://github.com/kkamkou/jqFenster\n' +
-        ' */\n';
+  return '/**\n' +
+    ' * jqFenster - Lightweight Modal Framework\n' +
+    ' * Version: ' + version + ' (' + buildDate + ')\n' +
+    ' * https://github.com/kkamkou/jqFenster\n' +
+    ' */\n';
 }
 
 // tasks
@@ -51,76 +52,76 @@ task('default', function () { console.log("Run `jake --tasks`\n"); });
 
 desc('build jqFenster')
 task({
-    'build': ['production/jquery.fenster.css', 'production/jquery.fenster.js']},
-    function(){
-        console.log('jqFenster build complete');
-    }
+  'build': ['production/jquery.fenster.css', 'production/jquery.fenster.js']},
+  function(){
+    console.log('jqFenster build complete');
+  }
 );
 
 desc('build jquery.fenster.full.css')
 file(
-    {'production/jquery.fenster.full.css': files['jquery.fenster.full.css']},
-    function(){
-        console.log('creating ' + this.name);
+  {'production/jquery.fenster.full.css': files['jquery.fenster.full.css']},
+  function(){
+    console.log('creating ' + this.name);
 
-        var data = '';
-        for (f in this.prereqs) {
-            file = this.prereqs[f];
-            console.log('\t' + file);
-            data += '\n/* File: ' + file + ' */\n';
-            data += fs.readFileSync(file);
-        }
-
-        fs.writeFileSync(this.name, getComment() + data);
+    var data = '';
+    for (f in this.prereqs) {
+      file = this.prereqs[f];
+      console.log('\t' + file);
+      data += '\n/* File: ' + file + ' */\n';
+      data += fs.readFileSync(file);
     }
+
+    fs.writeFileSync(this.name, getComment() + data);
+  }
 );
 
 // Css
 desc('optimize jquery.fenster.full.css');
 file(
-    {'production/jquery.fenster.css': ['production/jquery.fenster.full.css']},
-    function () {
-        console.log('optimizing jquery.fenster.css');
+  {'production/jquery.fenster.css': ['production/jquery.fenster.full.css']},
+  function () {
+    console.log('optimizing jquery.fenster.css');
 
-        var css_optimized = csso.justDoIt(fs.readFileSync('production/jquery.fenster.full.css').toString())
-        fs.writeFileSync(this.name, getComment() + css_optimized);
-    }
+    var css_optimized = csso.justDoIt(fs.readFileSync('production/jquery.fenster.full.css').toString())
+    fs.writeFileSync(this.name, getComment() + css_optimized);
+  }
 );
 
 desc('build jquery.fenster.full.js')
 file(
-    {'production/jquery.fenster.full.js': files['jquery.fenster.full.js']},
-    function () {
-        console.log('building jquery.fenster.full.js');
+  {'production/jquery.fenster.full.js': files['jquery.fenster.full.js']},
+  function () {
+    console.log('building jquery.fenster.full.js');
 
-        var strict = new RegExp('"use strict"\;?\n?'),
-            jqFensterSet = files['jquery.fenster.full.js'];
+    var strict = new RegExp('"use strict"\;?\n?'),
+      jqFensterSet = files['jquery.fenster.full.js'];
 
-        var data = '';
-        for (f in jqFensterSet) {
-            file = jqFensterSet[f];
-            console.log('\t' + file);
-            data += '\n\n/*\n * File: ' + file + '\n */\n\n';
-            data += fs.readFileSync(file);
-            data = data.replace(strict, '');
-        }
-        data = '(function($) {\n' + data + '\n})(jQuery);'; // add closure
-        fs.writeFileSync(this.name, getComment() + data);
+    var data = '';
+    for (f in jqFensterSet) {
+      file = jqFensterSet[f];
+      console.log('\t' + file);
+      data += '\n\n/*\n * File: ' + file + '\n */\n\n';
+      data += fs.readFileSync(file);
+      data = data.replace(strict, '');
     }
+    data = '(function($) {\n' + data + '\n})(jQuery);'; // add closure
+    fs.writeFileSync(this.name, getComment() + data);
+  }
 );
 
 // Js
 desc('uglify jquery.fenster.js');
 file(
-    {'production/jquery.fenster.js': ['production/jquery.fenster.full.js']},
-    function () {
-        console.log('uglify jquery.fenster.js');
+  {'production/jquery.fenster.js': ['production/jquery.fenster.full.js']},
+  function () {
+    console.log('uglify jquery.fenster.js');
 
-        var ast = ugp.parse(fs.readFileSync('production/jquery.fenster.full.js').toString());
-        ast = ugu.ast_mangle(ast);
-        ast = ugu.ast_squeeze(ast);
+    var ast = ugp.parse(fs.readFileSync('production/jquery.fenster.full.js').toString());
+    ast = ugu.ast_mangle(ast);
+    ast = ugu.ast_squeeze(ast);
 
-        var result = ugu.split_lines(ugu.gen_code(ast), 1024 * 8);
-        fs.writeFileSync(this.name, getComment() + result);
-    }
+    var result = ugu.split_lines(ugu.gen_code(ast), 1024 * 8);
+    fs.writeFileSync(this.name, getComment() + result);
+  }
 );
