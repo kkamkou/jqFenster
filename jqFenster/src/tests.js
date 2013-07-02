356d6e9923afe54626be4b44dd3e52a8ad188f21
Kanstantsin Kamkou <kkamkou@gmail.com>
2013-07-02 17:04:43 +0200

    tests changed

--- jqFenster/src/tests.js
+++ jqFenster/src/tests.js
@@ -1,5 +1,3 @@
-QUnit.config.reorder = false;
-
 test('DOM Init', 2, function() {
   equal($('.jqFensterHolder').length, 0, 'DOM does not have jqFensterHolder');
   equal($('.jqEbony').length, 0, 'DOM does not have jqEbony');
@@ -22,7 +20,7 @@ test('API Check (jquery.fenster.js)', 8, function() {
 });
 
 module('In-DOM instance');
-asyncTest('open()', 3, function() {
+asyncTest('open()/close()', 6, function() {
   var $fenster = $('#targetSecond').fenster().open();
 
   setTimeout(function () {
@@ -32,45 +30,35 @@ asyncTest('open()', 3, function() {
     equal($target.data('jqFensterAncestor').length, 1, 'Holder has the "jqFensterAncestor" data');
     deepEqual($fenster.getAncestor().get(0), $('#targetSecond').get(0), 'Elements are identical');
 
-    start();
-  }, 200);
-});
+    $.fensterFinder($('.jqFensterHolder')).close();
 
-asyncTest('close()', 3, function() {
-  $.fensterFinder($('.jqFensterHolder')).close();
+    setTimeout(function () {
+      var $elem = $('#targetSecond');
 
-  setTimeout(function () {
-    var $elem = $('#targetSecond');
+      equal($('.jqFensterHolder').length, 0, 'DOM does not have the "jqFensterHolder" element');
+      deepEqual($.type($elem.data('jqFensterLocked')), 'undefined', 'No jqFensterLocked in data');
+      deepEqual($.type($elem.data('jqFensterHolder')), 'undefined', 'No jqFensterHolder in data');
 
-    equal($('.jqFensterHolder').length, 0, 'DOM does not have the "jqFensterHolder" element');
-    deepEqual($.type($elem.data('jqFensterLocked')), 'undefined', 'No jqFensterLocked in data');
-    deepEqual($.type($elem.data('jqFensterHolder')), 'undefined', 'No jqFensterHolder in data');
-    start();
+      start();
+    }, 200);
   }, 200);
 });
 
 module('Dynamic instance');
-asyncTest('open()', 2, function() {
-  var countBefore = $('body a.jqFenster:hidden').length;
-
+asyncTest('open()/close()', 4, function() {
   $.fenster({'href': 'remote.html'}).open();
 
   setTimeout(function () {
-    equal(countBefore + 1, $('body a.jqFenster:hidden').length, 'Empty link added to the body');
+    equal($('body a.jqFenster:hidden').length, 2, 'Empty link added to the body');
     equal($('.jqFensterHolder').length, 1, 'DOM has the "jqFensterHolder" element');
-    start();
+
+    $.fensterFinder($('.jqFensterHolder')).close().destroy();
+
+    setTimeout(function () {
+      equal($('.jqFensterHolder').length, 0, 'DOM does not have the "jqFensterHolder" element');
+      equal($('body a.jqFenster:hidden').length, 1, 'Empty link removed from the body');
+
+      start();
+    }, 200);
   }, 200);
 });
-
-asyncTest('close()', 2, function() {
-  var countBefore = $('body a.jqFenster:hidden').length;
-
-  $.fensterFinder($('.jqFensterHolder')).close().destroy();
-
-  setTimeout(function () {
-    equal($('.jqFensterHolder').length, 0, 'DOM does not have the "jqFensterHolder" element');
-    equal(countBefore - 1, $('body a.jqFenster:hidden').length, 'Empty link removed from the body');
-    start();
-  }, 200);
-});
-
